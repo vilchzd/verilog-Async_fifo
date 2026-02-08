585bfd84e05026eec1c08bad3b4c2a1630b4eb21
vilchez <dievilchez23@gmail.com>
2026-02-08 02:42:45 -0600

    add tweaks to handlers

--- async_fifo.srcs/sources_1/new/async_read.v
+++ async_fifo.srcs/sources_1/new/async_read.v
@@ -3,6 +3,7 @@
 
 module async_read #(parameter ADDRESS_WIDTH = 4)(
     input wire r_clk,  
+    input wire r_rstn,
     input wire [ADDRESS_WIDTH-1:0] write_ptr_gsync,
     
     output reg [ADDRESS_WIDTH-1:0] read_ptr,
@@ -10,10 +11,11 @@ module async_read #(parameter ADDRESS_WIDTH = 4)(
     output reg empty
     );
     
-    
-always @ (posedge w_clk) begin 
-    if (w_en && !full) begin
-        write_ptr <= 0;
+always @ (posedge r_clk) begin 
+    if (!r_rstn) begin
+        read_ptr <= 0;
+        read_ptr_gray <= 0;
+    end else begin
         
     end
 end 
--- async_fifo.srcs/sources_1/new/async_write.v
+++ async_fifo.srcs/sources_1/new/async_write.v
@@ -2,19 +2,24 @@
 
 module async_write #(parameter ADDRESS_WIDTH = 4)(
     input wire w_clk,
-    input wire [ADDRESS_WIDTH-1:0] read_ptr_gsync,
+    input wire w_rstn,
+    input wire [ADDRESS_WIDTH-1:0] read_ptr_gsync,//sync read pointer in gray code
     
-    output reg [ADDRESS_WIDTH-1:0] write_ptr,
+    output reg [ADDRESS_WIDTH-1:0] write_ptr, 
     output reg [ADDRESS_WIDTH-1:0] write_ptr_gray,
     output reg full
     );
     
     
 always @ (posedge w_clk) begin 
-    if (w_en && !full) begin
+    if (!w_rstn) begin
         write_ptr <= 0;
+        write_ptr_gray <= 0;
+    end else begin
         
     end
 end 
+
+assign write_ptr = 
 
 endmodule
